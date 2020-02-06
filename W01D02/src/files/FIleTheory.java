b59fb168091a10d462c6d1e0cb3f02ce24636340
kamkali <kamkalis@gmail.com>
2020-02-06 12:53:42 +0100

    Ex7/8 done

--- W01D02/src/files/FIleTheory.java
+++ W01D02/src/files/FIleTheory.java
@@ -10,29 +10,53 @@ public class FileTheory {
 
         getIfnoAboutNewFile();
         ex7();
+        ex8();
+    }
+
+    private static void ex8() {
+        System.out.println("8. Podaj sciezke do pliku: ");
+        System.out.println("~$ ");
+
+        Scanner scanner = new Scanner(System.in);
+        String path = scanner.nextLine();
+        try (Scanner fileScanner = new Scanner(new File(path))){
+            int lineCounter = 0;
+            while (fileScanner.hasNext()){
+                lineCounter++;
+                System.out.println(fileScanner.nextLine());
+            }
+            System.out.println("Ilosc lini w pliku: " + lineCounter);
+        } catch (IOException e) {
+            System.out.println("Error!");
+            e.printStackTrace();
+        }
     }
 
     private static void ex7(){
-        System.out.println("Podaj sciezke do pliku: ");
+        System.out.println("7. Podaj sciezke do pliku: ");
         System.out.println("~$ ");
         String path;
         Scanner scanner = new Scanner(System.in);
         path = scanner.nextLine();
 
         try(FileOutputStream file = new FileOutputStream(path)) {
-            do {
+            while (true){
                 System.out.println("Podaj linie do wpisania do pliku: ");
                 System.out.println("~$ ");
+                String buf = scanner.nextLine();
+                if (buf.equals("-"))
+                    break;
 
-                byte[] byteArr = scanner.nextLine().getBytes();
+                byte[] byteArr = buf.getBytes();
+
                 file.write(byteArr);
                 file.write("\n".getBytes());
+            }
 
-            } while (!scanner.nextLine().equals("-"));
+
         } catch (NullPointerException | IOException e){
             System.out.println("Nie udalo sie!");
         }
-
     }
 
 
