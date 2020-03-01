4e9889c880f9ec0eaa5183433e58612173bbd77e
kamkali <kamkalis@gmail.com>
2020-03-01 18:21:53 +0100

    W3D2: Spring basics

--- W03D02/src/main/java/com/kurs/spring/spring_app/DataController.java
+++ W03D02/src/main/java/com/kurs/spring/spring_app/DataController.java
@@ -1,4 +1,51 @@
 package com.kurs.spring.spring_app;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
 public class DataController {
+
+    private Employee employee = new Employee("Kamil", 22);
+    private List<Employee> employees = new ArrayList<>();
+
+    @GetMapping("/employees")
+    @ResponseBody
+    public ResponseEntity<List<Employee>> getEmployeesList() {
+        if(!employees.contains(employee))
+            employees.add(employee);
+
+        return ResponseEntity.status(HttpStatus.OK).body(employees);
+    }
+
+    @GetMapping("/employee/{id}")
+    public ResponseEntity<Employee> getEmployee(@PathVariable("id") Integer id) {
+        return ResponseEntity.status(HttpStatus.OK).body(this.employees.get(id));
+    }
+
+    @PutMapping("/employee/{id}")
+    public ResponseEntity<Employee> putEmployee(@PathVariable("id") Integer id,
+                                                @RequestBody Employee employee) {
+
+        this.employees.get(id).setAge(employee.getAge());
+        this.employees.get(id).setName(employee.getName());
+
+        return ResponseEntity.status(HttpStatus.OK).body(employees.get(id));
+    }
+
+
+    @PostMapping("/employee")
+//    @ResponseBody     // with @RestController works without @ResponseBody
+    public ResponseEntity<List<Employee>> postEmployeesList(@RequestBody Employee employee) {
+        employee.setId(this.employees.size());
+        employees.add(employee);
+
+        return ResponseEntity.status(HttpStatus.OK).body(employees);
+    }
 }
---
+++ W03D02/src/main/java/com/kurs/spring/spring_app/Employee.java
@@ -0,0 +1,36 @@
+package com.kurs.spring.spring_app;
+
+public class Employee {
+    private int id;
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
