99668d1c897ce6554b079360e28830efb132fece
kamkali <kamkalis@gmail.com>
2020-03-07 09:54:25 +0100

    W4D1: Init commit

---
+++ moviestorage/src/main/java/com/kurs/moviestorage/MoviestorageApplication.java
@@ -0,0 +1,13 @@
+package com.kurs.moviestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviestorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviestorageApplication.class, args);
+    }
+
+}
---
+++ moviestorage/src/test/java/com/kurs/moviestorage/MoviestorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurs.moviestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviestorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
